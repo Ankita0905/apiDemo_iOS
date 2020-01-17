88ca0707c7e1f738c938772e69bdb06b28432b04
Ankita Jain <ankita.jain12342@gmail.com>
2020-01-17 16:30:15 -0500

    init

--- apiDemo_iOS/ViewController.swift
+++ apiDemo_iOS/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cityTxt: UITextField!
+    @IBOutlet weak var weatherLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let url=URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e374da6d1dfad92ef07f72798d1e50a1")!
+        
+    }
+
+    @IBAction func selectCityBtn(_ sender: UIButton) {
+        
+        let url=URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(cityTxt.text!.replacingOccurrences(of: " ", with: "%20"))&APPID=e374da6d1dfad92ef07f72798d1e50a1")!
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let error = error{
                 print(error)
@@ -25,7 +32,15 @@ class ViewController: UIViewController {
                 {
                     do{
                         let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
-                        print(jsonResult)
+                       // print(jsonResult)
+                        //print(jsonResult?["name"] as! String)
+                        if let description = ((jsonResult?["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String
+                        {
+                          //print(description)
+                            DispatchQueue.main.async {
+                                self.weatherLbl.text=description
+                            }
+                        }
                     }catch{
                         print(error)
                     }
@@ -36,7 +51,6 @@ class ViewController: UIViewController {
         //start the task
         task.resume()
     }
-
-
+    
 }
 
